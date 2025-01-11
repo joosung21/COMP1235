1572cde4cbca596ff5d85a977ae40ff1ffa0c448
Joosung Ahn <joosung21@gmail.com>
2025-01-10 20:54:23 -0500

    Initial commit

---
+++ public_html/comp1235/labs/week1/walkthrough1.js
@@ -0,0 +1,18 @@
+// Output on dialog box
+alert("Hello World!");
+// Output on console
+console.log("Hi, I am here!");
+// Output on the screen
+document.write("Welcome to the class C542!");
+
+// To ask something from user
+prompt("What is your name?");
+
+// declare a var
+let name = "ABC";
+let age = 7;
+const tax = 0.13;
+// array
+// FRUITS
+let fruits = ["apple", "banana", "orange"];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
